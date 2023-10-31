3f4303f525bc1ffba5efadeae18e2de074c66847
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-10-31 14:09:27 +0100

    Add http-requests using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import './App.css'
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {Cat} from "./Cat.ts";
 
 function App() {
 
+    const [cat, setCat] = useState<Cat>();
+    const [story, setStory] = useState<string>();
+
+    useEffect(() => {
+        axios.get("/api/cats/58c5ac6c-dc3e-426d-928e-4351b4b12827")
+            .then(response => setCat(response.data))
+        axios.get("/api/cats/58c5ac6c-dc3e-426d-928e-4351b4b12827/story")
+            .then(response => setStory(response.data))
+    }, []);
+
+    function createBob() {
+        axios.post("/api/cats", {
+            "id": "123671523817-234234",
+            "name": "Bob",
+            "breed": "Siam",
+            "age": 14
+        })
+    }
+
     return (
         <>
-            Unsere Katze
+            <button onClick={createBob}>Lege Katze Bob an</button>
+
+            Unsere Katze {cat?.name} sie ist {cat?.age} Jahre alt und eine {cat?.breed}-Katze.
+
+            {
+                story ? <>
+                    Ihre Story:
+                    {story}
+                </> : <>Lade story...</>
+            }
         </>
     )
 }
---
+++ src/Cat.ts
@@ -0,0 +1,6 @@
+export type Cat = {
+    id: string,
+    name: string,
+    breed: string,
+    age: number,
+}
